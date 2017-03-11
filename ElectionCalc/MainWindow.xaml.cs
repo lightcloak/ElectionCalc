edcb05708fbb62f35ec4437deac4873abfcc4d1d
odin <milosz.odin@gmail.com>
2017-03-12 00:08:46 +0100

    Base version.

---
+++ ElectionCalc/MainWindow.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace ElectionCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        // Call to Action
+        private string nameCTA = "Input Your name...";
+        private string surnameCTA = "Input Your surname...";
+        private string peselCTA = "Input Your PESEL...";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void logIn(string name, string surname, string pesel)
+        {
+            var loggedPerson = new Voter
+            {
+                Name = name,
+                Surname = surname,
+                Pesel = Convert.ToInt64(pesel)
+            };
+
+            WorkWindow window2 = new WorkWindow(loggedPerson);
+            window2.Show();
+            this.Close();
+        }
+
+        // ======================================================================
+        // Akcje
+        // ======================================================================
+
+        private void tboxName_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tboxName.Text == nameCTA)
+            {
+                tboxName.Text = string.Empty;
+            }
+        }
+
+        private void tboxName_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrWhiteSpace(tboxName.Text))
+            {
+                tboxName.Text = nameCTA;
+            }
+        }
+
+        private void tboxSurname_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if(tboxSurname.Text == surnameCTA)
+            {
+                tboxSurname.Text = string.Empty;
+            }
+        }
+
+        private void tboxSurname_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tboxSurname.Text))
+            {
+                tboxSurname.Text = surnameCTA;
+            }
+        }
+
+        private void tboxPesel_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (tboxPesel.Text == peselCTA)
+            {
+                tboxPesel.Text = string.Empty;
+            }
+        }
+
+        private void tboxPesel_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tboxPesel.Text))
+            {
+                tboxPesel.Text = peselCTA;
+            }
+        }
+
+        private void tboxPesel_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            // Ograniczenie wartosci jakie moga byc wprowadzone w pole
+            // Inny sposob: IsDigit
+            // ((e.Key >= Key.A && e.Key <= Key.Z) to litery
+            if ((e.Key >= Key.D0 && e.Key <= Key.D9) ||
+                (e.Key >= Key.NumPad0 && e.Key <= Key.NumPad9) ||
+                 e.Key == Key.Back ||
+                 e.Key == Key.Delete ||
+                 e.Key == Key.Enter)
+            {
+                if(e.Key == Key.Enter)
+                {
+                    btnLogIn_Click(null, null);
+                }
+                else
+                    return;
+            }
+            else
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnLogIn_Click(object sender, RoutedEventArgs e)
+        {
+            // Do zaimplementowania metoda prezentacji pol, ktore nie przeszly walidacji
+            // https://mndevnotes.wordpress.com/2012/12/01/wpf-walidacja-danych/
+
+            // Blokada na czas przetwarzania pola
+            tboxName.IsEnabled = false;
+            tboxSurname.IsEnabled = false;
+            tboxPesel.IsEnabled = false;
+
+            string name = tboxName.Text;
+            string surname = tboxSurname.Text;
+            string pesel = tboxPesel.Text;
+
+            if (!string.IsNullOrWhiteSpace(name) &&
+                                 name != nameCTA &&
+
+                !string.IsNullOrWhiteSpace(surname) &&
+                              surname != surnameCTA &&
+
+                !string.IsNullOrWhiteSpace(pesel) &&
+                                pesel != peselCTA &&
+
+                 Validators.IsValidPESEL(pesel) &&
+                 Validators.IsOldEnough(pesel))
+            {
+                // przekazanie parametrow w troche mniej popularny sposob
+                logIn(name: name, surname: surname, pesel: pesel);
+            }
+            else
+            {
+                MessageBox.Show("Please check input fields for errors." +
+                                "\nBelow are some common problems:" +
+                                "\n\nName, Surname and Pesel has to be filled in." +
+                                "\nProvided Pesel number has to be valid." +
+                                "\nYou have to be at least 18 years old in order to Log in.",
+                                "We've detected some problems");
+                // Zwolnienie blokady
+                tboxName.IsEnabled = true;
+                tboxSurname.IsEnabled = true;
+                tboxPesel.IsEnabled = true;
+            }
+        }
+    }
+}
---
+++ ElectionCalc/Validators.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectionCalc
+{
+    static class Validators
+    {
+        // funkcja pochodzi z adresu: http://www.extensionmethod.net/2004/csharp/string/isvalidnip-isvalidregon-isvalidpesel
+        public static bool IsValidPESEL(string input)
+        {
+            int[] weights = { 1, 3, 7, 9, 1, 3, 7, 9, 1, 3 };
+            bool result = false;
+            if (input.Length == 11)
+            {
+                int controlSum = CalculateControlSum(input, weights);
+                int controlNum = controlSum % 10;
+                controlNum = 10 - controlNum;
+                if (controlNum == 10)
+                {
+                    controlNum = 0;
+                }
+                int lastDigit = int.Parse(input[input.Length - 1].ToString());
+                result = controlNum == lastDigit;
+            }
+            return result;
+        }
+
+        // funkcja pochodzi z adresu: http://www.extensionmethod.net/2004/csharp/string/isvalidnip-isvalidregon-isvalidpesel
+        private static int CalculateControlSum(string input, int[] weights, int offset = 0)
+        {
+            int controlSum = 0;
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                controlSum += weights[i + offset] * int.Parse(input[i].ToString());
+            }
+            return controlSum;
+        }
+
+        public static bool IsOldEnough(string input)
+        {
+            bool result = false;
+            // Pobieramy numer pesel w celu otrzymania roku, miesiaca, dnia
+            string yearmonthday = new string(input.Take(6).ToArray());
+            int year = int.Parse(yearmonthday.Substring(0, 2));
+            int month = int.Parse(yearmonthday.Substring(2, 2));
+            int day = int.Parse(yearmonthday.Substring(4, 2));
+
+            // Malo wydajna obsluga roku >= 2000
+            if (month > 20)
+            {
+                month = int.Parse((month - 20).ToString("D2"));
+                year = year + 2000;
+            }
+            else
+            {
+                year = year + 1900;
+            }
+
+            DateTime birth = new DateTime(year, month, day);
+
+            Age age = new Age(birth);
+
+            if (age.Years >= 18)
+            {
+                result = true;
+            }
+            // Malo eleganckie wyzerowanie referencji na obiekt
+            // Singleton bylby tutaj lepszym wyjsciem
+            age = null;
+            return result;
+        }
+    }
+}
